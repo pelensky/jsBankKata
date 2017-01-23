822a35941770a366b405f65d11aef9485d22ccff
Dan Pelensky <dan@pelensky.com>
2017-01-23 10:28:56 +0000

    empty transactions array

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -5,12 +5,16 @@ describe("Account", function() {
   });
 
   it("should initialize with a balance of zero", function() {
-
     expect(account.currentBalance()).toEqual(0);
+  });
+
+  it("should initialize with no transactions", function(){
+    expect(account.transactions).toEqual([]);
   });
 
   it("should allow deposits to add to the balance", function() {
     account.depositFunds(1000);
     expect(account.currentBalance()).toEqual(1000);
   });
+
 });
--- src/Account.js
+++ src/Account.js
@@ -1,5 +1,6 @@
 function Account() {
   this.balance = 0;
+  this.transactions = [];
 }
 Account.prototype.currentBalance = function() {
   return this.balance;
